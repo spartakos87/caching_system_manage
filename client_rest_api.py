543c40ebe61d3bbfa68179cf021ca74f23c2625e
spartakos <serepasf@gmail.com>
2020-05-21 21:19:48 +0300

    remove timestamp, random choose of proxy server and implement on demand caching algorithm

--- client_rest_api.py
+++ client_rest_api.py
@@ -11,6 +11,7 @@ cache_dict = {}
 controller_ip = ""
 client_ip = ""
 
+
 class ClientCachingDB(Resource):
     def request_controller(self, url, client_ip):
         """
@@ -19,34 +20,40 @@ class ClientCachingDB(Resource):
         """
         controller_data = post(controller_ip, json={"url": url, "client_ip": client_ip}).json()
         cache_dict = controller_data["cache_dict"]
-        START_TIME = controller_data["START_TIME"]
+        # START_TIME = controller_data["START_TIME"]
         proxy_url = controller_data["proxy_url"]
         return cache_dict, START_TIME, proxy_url
 
     def post(self):
+        # global START_TIME, cache_dict, CACHE_TIME
         global START_TIME, cache_dict, CACHE_TIME
         post_data = request.get_json()
         if post_data.get("request_url", None):
             url = post_data["request_url"]
             if not bool(cache_dict):
                 # cache dict is empty
-                cache_dict, START_TIME, proxy_url = self.request_controller(url, client_ip)
+                # cache_dict, START_TIME, proxy_url = self.request_controller(url, client_ip)
+                cache_dict, proxy_url = self.request_controller(url, client_ip)
             else:
-                if time() - START_TIME < CACHE_TIME:
-                    # START_TIME is not expired
-                    if cache_dict.get(url, None):
-                        # The asking url exist in cache_dict
-                        # First check if its timestamp is not expired
-                        timestamp = cache_dict[url]["timestamp"]
-                        if time() - timestamp < CACHE_TIME:
-                            proxy_url = cache_dict[url]["proxy_url"]
-                        else:
-                            cache_dict, START_TIME, proxy_url = self.request_controller(url, client_ip)
-                    else:
-                        cache_dict, START_TIME, proxy_url = self.request_controller(url, client_ip)
+                if cache_dict.get(url, None):
+                    proxy_url = cache_dict[url]["proxy_url"]
                 else:
-                    # START_TIME is expired get new cache_dict and new START_TIME
-                    cache_dict, START_TIME, proxy_url = self.request_controller(url, client_ip)
+                    cache_dict, proxy_url = self.request_controller(url, client_ip)
+                # if time() - START_TIME < CACHE_TIME:
+                #     # START_TIME is not expired
+                #     if cache_dict.get(url, None):
+                #         # The asking url exist in cache_dict
+                #         # First check if its timestamp is not expired
+                #         timestamp = cache_dict[url]["timestamp"]
+                #         if time() - timestamp < CACHE_TIME:
+                #             proxy_url = cache_dict[url]["proxy_url"]
+                #         else:
+                #             cache_dict, START_TIME, proxy_url = self.request_controller(url, client_ip)
+                #     else:
+                #         cache_dict, START_TIME, proxy_url = self.request_controller(url, client_ip)
+                # else:
+                #     # START_TIME is expired get new cache_dict and new START_TIME
+                #     cache_dict, START_TIME, proxy_url = self.request_controller(url, client_ip)
         else:
             raise Exception("Something goes wrong")
         return {
--- controller_rest_api.py
+++ controller_rest_api.py
@@ -1,19 +1,72 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from time import time
+from random import randint
+from math import factorial, pow
+from requests import get
 
 app = Flask(__name__)
 api = Api(app)
-START_TIME = 0
+# START_TIME = 0
 CACHE_TIME = 10
 cache_dict = {}
 # TODO parse proxy_to_clients as file or as argument
 proxy_to_clients = {}
+proxy_list = []
+LEN_PROXIES = len(proxy_to_clients)
+THREHOLD = 0.8
+LAMBDA = 30
+E = 2.718
 
 
 class CachingDB(Resource):
+    def __init__(self):
+        self.on_deman_caching_datastructure = {}  # A dict with key the URI and R the rate (# of requests )
+        self.proxy_list = []
+        self.LEN_PROXIES = len(proxy_to_clients)
+        self.cache_dict = {}
+        self.THREHOLD = 0.8
+        self.LAMBDA = 30
+        self.E = 2.718
+
+
+    def popularity_probability(self, r):
+        """
+
+        :param R:
+        :return:
+        """
+        g = 0
+        for i in range(r+1):
+            self.g += (pow(self.LAMBDA, i) * pow(self.E, -self.LAMBDA)) / factorial(i)
+        return g
+
+    def on_demand_caching(self, uri):
+        """
+        Implement the On-demand caching algorithm base to paper,
+         DOI: http://dx.doi.org/10.1145/2836183.2836189 (section 3.1 )
+
+        :return:
+        """
+        if bool(self.on_deman_caching_datastracture):
+            # Case the data structure is not empty
+            if self.on_deman_caching_datastructure.get(url, None):
+                r = self.on_deman_caching_datastructure[uri]
+                if self.popularity_probability(R+1) > self.THREHOLD:
+                    for proxy in self.proxy_list:
+                        proxy = {"http": proxy}
+                        res_via_proxy = get(uri, proxies=proxy)
+                        if res_via_proxy.status_code == 200:
+                            print("Cach object {} in proxy {}".format(uri, proxy))
+                            # Update self.cache_dict
+                            self.update_cache_dict(uri, proxy)
+                        else:
+                            print("Something goes wrong")
+                else:
+                    self.on_deman_caching_datastructure[uri] = r + 1
 
     def clean_cache_dict(self, cache_dict):
+        # TODO remove it we don't need it
         """
         Remove the caching objects which theis timestamps are expired
 
@@ -26,18 +79,23 @@ class CachingDB(Resource):
         except:
             return cache_dict
 
-    def export_data(self, proxy_url, cache_dict, START_TIME):
+    # def export_data(self, proxy_url, cache_dict, START_TIME):
+    def export_data(self, proxy_url):
         """
 
-        :param proxy_url:
+        :param proxy_url: a list of proxies
         :param cache_dict:
         :param START_TIME:
         :return:
         """
+        if len(proxy_url) == 1:
+            proxy_url = proxy_url[0]
+        else:
+            proxy_url = proxy_url[randint(0, len(proxy_url))]
         return {
                  "proxy_url": proxy_url,
-                 "cache_dict": cache_dict,
-                 "START_TIME": START_TIME
+                 "cache_dict": self.cache_dict
+                 # "START_TIME": START_TIME
                                     }
 
     def update_cache_dict(self, request_url, proxy_url):
@@ -47,12 +105,14 @@ class CachingDB(Resource):
         :param proxy_url:
         :return:
         """
-        return {
-                    request_url: {
-                                    "timestamp": time(),
-                                    "proxy_url": proxy_url
-                                                }
-                    }
+        temp_proxies_list = self.cache_dict[request_url]
+        self.cache_dict[request_url] = temp_proxies_list.append(proxy_url)
+        # return {
+        #             request_url: {
+        #                             # "timestamp": time(),
+        #                             "proxy_url": proxy_url
+        #                                         }
+        #             }
 
     def post(self):
         """
@@ -60,48 +120,67 @@ class CachingDB(Resource):
         As input will get a dict with key the url which client is requested and the ip of client
         :return:
         """
-        global START_TIME, cache_dict
+        # global START_TIME, cache_dict
         post_data = request.get_json()
         request_url = post_data.get("url", None)
-        client_ip = post_data.get("client_ip", None)
+        # Call on demand caching
+        self.on_demand_caching(request_url)
+        # client_ip = post_data.get("client_ip", None)
         if request_url is None:
             raise Exception("There isn't url")
         # Check if cache_dict is empty
-        if bool(cache_dict):
+        if bool(self.cache_dict):
             # cache_dict is not empty
-            if time() - START_TIME < CACHE_TIME:
-                # The START_TIME is not expired
-                # Check if the request url is in cache_dict
-                if cache_dict.get(request_url, None):
-                    # First check if its timestamp is not expired
-                    timestamp = cache_dict[request_url]["timestamp"]
-                    if time() - timestamp < CACHE_TIME:
-                        # Is not expired
-                        return_json = self.export_data(cache_dict[request_url]["proxy_url"], cache_dict, START_TIME)
-                    else:
-                        # Has expired
-                        proxy_url = proxy_to_clients.get(client_ip, None)
-                        cache_dict.update(self.update_cache_dict(request_url, proxy_url))
-                else:
-                    proxy_url = proxy_to_clients.get(client_ip, None)
-                    cache_dict.update(self.update_cache_dict(request_url, proxy_url))
-                    return_json = self.export_data(proxy_url, cache_dict, START_TIME)
+            # Check if the request url is in cache_dict
+            if self.cache_dict.get(request_url, None):
+                # return_json = self.export_data(self.cache_dict[request_url]["proxy_url"], self.cache_dict)
+                return_json = self.export_data(self.cache_dict[request_url]["proxy_url"])
             else:
-                # The START_TIME is expired
-                # Clean cache_dict
-                cache_dict = self.clean_cache_dict(cache_dict)
-                if cache_dict.get(request_url, None):
-                    return_json = self.export_data(cache_dict[request_url]["proxy_url"], cache_dict, START_TIME)
-                else:
-                    proxy_url = proxy_to_clients.get(client_ip, None)
-                    cache_dict.update(self.update_cache_dict(request_url, proxy_url))
-                    return_json = self.export_data(proxy_url, cache_dict, START_TIME)
+                # Get random proxy
+                proxy_url = self.proxy_list[randint(0, self.LEN_PROXIES)]
+                # proxy_url = proxy_to_clients.get(client_ip, None)
+                # START_TIME = time()
+                # self.cache_dict.update(self.update_cache_dict(request_url))
+                self.update_cache_dict(request_url, proxy_url)
+                # return_json = self.export_data(proxy_url, self.cache_dict)
+                return_json = self.export_data(proxy_url)
+            # if time() - START_TIME < CACHE_TIME:
+            #     # The START_TIME is not expired
+            #     # Check if the request url is in cache_dict
+            #     if cache_dict.get(request_url, None):
+            #         # First check if its timestamp is not expired
+            #         timestamp = cache_dict[request_url]["timestamp"]
+            #         if time() - timestamp < CACHE_TIME:
+            #             # Is not expired
+            #             return_json = self.export_data(cache_dict[request_url]["proxy_url"], cache_dict, START_TIME)
+            #         else:
+            #             # Has expired
+            #             proxy_url = proxy_to_clients.get(client_ip, None)
+            #             cache_dict.update(self.update_cache_dict(request_url, proxy_url))
+            #     else:
+            #         proxy_url = proxy_to_clients.get(client_ip, None)
+            #         cache_dict.update(self.update_cache_dict(request_url, proxy_url))
+            #         return_json = self.export_data(proxy_url, cache_dict, START_TIME)
+            # else:
+            #     # The START_TIME is expired
+            #     # Clean cache_dict
+            #     cache_dict = self.clean_cache_dict(cache_dict)
+            #     if cache_dict.get(request_url, None):
+            #         return_json = self.export_data(cache_dict[request_url]["proxy_url"], cache_dict, START_TIME)
+            #     else:
+            #         proxy_url = proxy_to_clients.get(client_ip, None)
+            #         cache_dict.update(self.update_cache_dict(request_url, proxy_url))
+            #         return_json = self.export_data(proxy_url, cache_dict, START_TIME)
         else:
             # The cache_dict is empty
-            proxy_url = proxy_to_clients.get(client_ip, None)
-            START_TIME = time()
-            cache_dict.update(self.update_cache_dict(request_url, START_TIME))
-            return_json = self.export_data(proxy_url, cache_dict, START_TIME)
+            # Get random proxy
+            proxy_url = self.proxy_list[randint(0, self.LEN_PROXIES)]
+            # proxy_url = proxy_to_clients.get(client_ip, None)
+            # START_TIME = time()
+            # self.cache_dict.update(self.update_cache_dict(request_url))
+            self.update_cache_dict(request_url, proxy_url)
+            # return_json = self.export_data(proxy_url, self.cache_dict)
+            return_json = self.export_data(proxy_url)
         return return_json
 
 
